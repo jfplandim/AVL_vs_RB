2d0b610587b5a02697af5e14d7420aa546e25c8a
Jose Landim <joselandimsobrinho@hotmail.com>
2026-05-15 16:29:08 -0300

    feat: implementando a remoção da árvore AVL

--- src/AVL_Router_Tree.java
+++ src/AVL_Router_Tree.java
@@ -109,4 +109,87 @@ public class AVL_Router_Tree {
         return (node != null) ? node.dado : null;
     }
 
+    //remoção
+    //buscamos o menor valor da subárvore direita(in-order)
+    private NodeAVL menorNo(NodeAVL node) {
+        NodeAVL atual = node;
+        while (atual.esquerdo != null)
+            atual = atual.esquerdo;
+        return atual;
+    }
+
+    public void remover(int prioridade) {
+        raiz = remover(raiz, prioridade);
+    }
+
+    private NodeAVL remover(NodeAVL node, int prioridade) {
+        //nó nao encontrado
+        if (node == null)
+            return null;
+        
+        //1. busca recursiva da bst
+        if (prioridade < node.dado.getPrioridade())
+            node.esquerdo = remover(node.esquerdo, prioridade);
+        else if (prioridade > node.dado.getPrioridade())
+            node.direito = remover(node.direito,  prioridade);
+        else {
+            //apos encontrar o nó
+            if (node.esquerdo == null || node.direito == null) {
+                NodeAVL temp = (node.esquerdo != null) ? node.esquerdo : node.direito;
+
+                //caso 1: folha
+                if (temp == null)
+                    node = null;
+                //caso 2: um filho
+                else
+                    node = temp;
+            } else {
+                //caso 3: 2 filhos - busca o sucessor in order
+                NodeAVL sucessor = menorNo(node.direito);
+
+                //copia o valor do sucessor para o nó atual
+                node.dado = sucessor.dado;
+
+                //remove o sucessor da subárvore direita
+                node.direito = remover(node.direito, sucessor.dado.getPrioridade());
+            }
+
+        }
+
+        //se a árvore tinha só 1 nó e foi removido
+        if (node == null)
+            return null;
+
+        //2. atualiza a altura
+        node.altura = 1 + Math.max(altura(node.esquerdo), altura(node.direito));
+
+        //3. calcula fator de balanceamento
+        int fb = fatorBalanceamento(node);
+
+        //4. rotações
+
+        //LL
+        if (fb > 1 && fatorBalanceamento(node.esquerdo) >= 0)
+            return rotacionarDireita(node);
+
+        //RR
+        if (fb < -1 && fatorBalanceamento(node.direito) <= 0)
+            return rotacionarEsquerda(node);
+
+        //LR
+        if (fb > 1 && fatorBalanceamento(node.esquerdo) < 0) {
+            node.esquerdo = rotacionarEsquerda(node.esquerdo);
+            return  rotacionarDireita(node);
+        }
+
+        //RL
+        if (fb < -1 && fatorBalanceamento(node.direito) > 0) {
+            node.direito = rotacionarDireita(node.direito);
+            return rotacionarEsquerda(node);
+        }
+
+        return node;
+
+    }
+
 }
