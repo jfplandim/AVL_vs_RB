857e7963a33a5ad7d18c71d3d982acadcb9d3fb0
Gustavo <gusta23316@gmail.com>
2026-05-25 09:58:15 -0300

    criação do geradorPacketRule

---
+++ src/geradorPackegerule.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+
+public class geradorPackegerule {
+   private static final long seed =1l;
+   private static final int enderecos=100_000;
+
+   private static String gerarIP(Random random){
+       return random.nextInt(256)+"." +
+               random.nextInt(256)+"."+
+               random.nextInt(256)+"."+
+               random.nextInt(256);
+   }
+
+   public static List<PacketRule> criarNo(){
+       Random random= new Random(seed);
+       List<PacketRule> regras= new ArrayList<>(enderecos);
+
+       for(int i=0;i<enderecos;i++){
+           String ipOrigem= gerarIP(random);
+           String ipDestino = gerarIP(random);
+           int prioridade= random.nextInt(100)+1;
+
+           regras.add(new PacketRule(i,ipOrigem,ipDestino,prioridade));
+       }
+       return regras;
+   }
+}
