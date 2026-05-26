f37522d4a35e043e51bc00ce9001351861c17ae8
Jose Landim <joselandimsobrinho@hotmail.com>
2026-05-26 09:39:26 -0300

    criando a classe da RBT e adicionando a ratoção a esquerda

---
+++ src/RedBlack_Router_Tree.java
@@ -0,0 +1,45 @@
+public class RedBlack_Router_Tree {
+    private NodeRBT raiz;
+    private NodeRBT nil;    //sentinela
+    private int rotacoes;
+
+    public RedBlack_Router_Tree() {
+        this.rotacoes = 0;
+        this.nil = new NodeRBT(null);   //dado do sentinela é null
+        this.nil.vermelho = false;            // sentinela é sempre preto
+
+        //ponteiros da sentinale apontam para ela
+        this.nil.esquerdo = this.nil;
+        this.nil.direito = this.nil;
+        this.nil.pai = this.nil;
+
+        //arvore nasce vazia, logo a raiz aponta para a sentinela
+        this.raiz = this.nil;
+    }
+
+    private void rotacionarEsquerda(NodeRBT x) {
+        NodeRBT y = x.direito; //define y como filho a direita de x
+        x.direito = y.esquerdo;
+
+        //transforma a subárvore esquerda de x na subarvore direita de x
+        if (y.esquerdo != this.nil) {
+            y.esquerdo.pai = x;
+        }
+
+        //liga o pai de x a y
+        y.pai = x.pai;
+        if (x.pai == this.nil) {    //se x era a raiz (pai é sentinela)
+            this.raiz =y;           //y passa a ser a nova raiz
+        } else if (x == x.pai.esquerdo) {
+            x.pai.esquerdo = y;
+        } else {
+            x.pai.direito = y;
+        }
+
+        //coloca x a esquerda de y
+        y.esquerdo = x;
+        x.pai = y;
+
+        rotacoes++;
+    }
+}
