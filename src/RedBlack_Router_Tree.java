6aae11d6fae51caad2b93106e11f501b34ae008b
Jose Landim <joselandimsobrinho@hotmail.com>
2026-05-26 09:45:35 -0300

    adicionando o metodo de rotacionar a direita

--- src/RedBlack_Router_Tree.java
+++ src/RedBlack_Router_Tree.java
@@ -42,4 +42,30 @@ public class RedBlack_Router_Tree {
 
         rotacoes++;
     }
+
+    private void rotacionarDireita(NodeRBT x){
+        NodeRBT y = x.esquerdo;     //define y como filho a esquerda de x
+        x.esquerdo = y.direito;
+
+        //transforma a subárvore direita de y na subárvore esquerda de x
+        if (y.direito != this.nil) {
+            y.direito.pai = x;
+        }
+
+        //liga o pai de x a y
+        y.pai = x.pai;
+        if (x.pai == this.nil) {    //se x era a raiz (pai é sentinela)
+            this.raiz = y;          //y passa a ser a nova raiz
+        } else if (x == x.pai.direito) {
+            x.pai.direito = y;
+        } else {
+            x.pai.esquerdo = y;
+        }
+
+        //coloca x a direita de y
+        y.direito = x;
+        x.pai =y;
+
+        rotacoes++;
+    }
 }
