81f4f68b044fe4a6ad08128f56852ee8ec8a40e1
Jose Landim <joselandimsobrinho@hotmail.com>
2026-05-26 09:37:49 -0300

    ajustes no nó da rbt

--- src/NodeRBT.java
+++ src/NodeRBT.java
@@ -5,11 +5,8 @@ public class NodeRBT {
     NodeRBT pai;
     boolean vermelho; //true = vermelho, false = preto
 
-    public NodeRBT(PacketRule dado, NodeRBT esquerdo, NodeRBT direito, NodeRBT pai, boolean vermelho) {
+    public NodeRBT(PacketRule dado) {
         this.dado = dado;
-        this.esquerdo = null;
-        this.direito = null;
-        this.pai = null;
-        this.vermelho = true; //todo no nasce vermelho
+        this.vermelho = true; //todo nó nasce vermelho
     }
 }
